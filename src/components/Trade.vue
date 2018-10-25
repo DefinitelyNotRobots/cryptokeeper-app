2dac190c3435489195f24ecf35b3dd4fde1d459d
Jack <ajackofsometrades@gmail.com>
2018-10-25 10:40:15 -0700

    bootstrap form

--- src/components/Trade.vue
+++ src/components/Trade.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <!-- <form>
     <label for="min">Minutes<br />
       <input type="number" v-model="minutes" name="time_m" id="min" min="0" max="59">
 		</label>
@@ -7,12 +7,53 @@
       <input type="number" v-model="seconds" name="time_s" id="sec" max="59" min="0">
     </label>
     <button type="button" @click="sendTime">Set time</button>
-  </form>
+  </form> -->
+  <b-form-group
+      horizontal
+      id="fieldset1"
+      description="Let's make a deal."
+      label="Enter your transaction details"
+      label-for="input1"
+      :invalid-feedback="invalidFeedback"
+      :valid-feedback="validFeedback"
+      :state="state"
+  >
+    <b-form-input id="input1" :state="state" v-model.trim="name"></b-form-input>
+  </b-form-group>
+  
 </template>
 
-<script>
-export default {
 
+
+
+<script>
+
+// select action: 
+    // select currency: 
+    // select exchange: 
+    // (show current price)
+    // number quantity
+
+
+export default {
+  computed: {
+    state() {
+      return this.name.length >= 4 ? true : false;
+    },
+    invalidFeedback() {
+      if(this.name.length > 4) return '';
+      else if (this.name.length > 0) return 'Enter at least 4 characters';
+      else return 'Please enter something';
+    },
+    validFeedback() {
+      return this.state === true ? 'Thank you' : '';
+    }
+  },
+  data() {
+    return {
+      name: ''
+    };
+  }
 };
 </script>
 
